7067fd05561fcb713339574741f127c781465267
daejo <daejo@icloud.com>
2020-09-28 22:40:39 -0700

    initializes and installs express

---
+++ server.js
@@ -0,0 +1,4 @@
+// # You can use the `-y` flag to skip the package questionnaire and leave default answers
+// npm init -y
+// # You can use `npm i` as a shortcut for `npm install`
+// npm i express
