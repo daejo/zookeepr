dcaa78bc2a49794c269572f1c3b02a379542b94b
daejo <daejo@icloud.com>
2020-09-28 23:07:22 -0700

    creates filter function

--- server.js
+++ server.js
@@ -7,10 +7,27 @@ const express = require("express");
 
 const app = express();
 
-
+function filterByQuery(query, animalsArray) {
+    let filteredResults = animalsArray;
+    if (query.diet) {
+      filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
+    }
+    if (query.species) {
+      filteredResults = filteredResults.filter(animal => animal.species === query.species);
+    }
+    if (query.name) {
+      filteredResults = filteredResults.filter(animal => animal.name === query.name);
+    }
+    return filteredResults;
+}
+  
 app.get('/api/animals', (req, res) => {
-    res.json(animals);
-})
+    let results = animals;
+    if (req.query) {
+      results = filterByQuery(req.query, results);
+    }
+    res.json(results);
+});
 
 //SHOULD BE AT THE END OF CODE, 3001 is localport//
 app.listen(3001, () => {
