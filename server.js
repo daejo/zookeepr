cc3c0799a83ba18eb323a6d2dba25b7d9c5f46e6
daejo <daejo@icloud.com>
2020-09-29 01:27:50 -0700

    adds validate animal function

--- server.js
+++ server.js
@@ -10,8 +10,14 @@
 const { animals } = require('./data/animals.json');
 const express = require("express");
 const PORT = process.env.PORT || 3001;
-
+const fs = require('fs');
+const path = require('path');
 const app = express();
+
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true })); //takes incoming POST data and converts it to key/value pairings that can be accessed in the req.body object
+// parse incoming JSON data
+app.use(express.json());
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -56,6 +62,32 @@ function findById(id, animalsArray) {
     const result = animalsArray.filter(animal => animal.id === id)[0];
     return result;
 }
+
+function createNewAnimal(body, animalsArray) {
+    const animal = body;
+    animalsArray.push(animal);
+    fs.writeFileSync(
+      path.join(__dirname, './data/animals.json'),
+      JSON.stringify({ animals: animalsArray }, null, 2)
+    );
+    return animal;
+}
+
+function validateAnimal(animal) {
+    if (!animal.name || typeof animal.name !== 'string') {
+      return false;
+    }
+    if (!animal.species || typeof animal.species !== 'string') {
+      return false;
+    }
+    if (!animal.diet || typeof animal.diet !== 'string') {
+      return false;
+    }
+    if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+      return false;
+    }
+    return true;
+}
   
 app.get('/api/animals', (req, res) => {
     let results = animals;
@@ -75,9 +107,13 @@ app.get('/api/animals/:id', (req, res) => {
 });
 
 app.post('/api/animals', (req, res) => {
-    // req.body is where our incoming content will be
-    console.log(req.body);
-    res.json(req.body);
+    // set id based on what the next index of the array will be
+    req.body.id = animals.length.toString();
+  
+    // add animal to json file and animals array in this function
+    const animal = createNewAnimal(req.body, animals);
+  
+    res.json(animal);
 });
 
 //SHOULD BE AT THE END OF CODE, 3001 is localport//
