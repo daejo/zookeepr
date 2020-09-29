620557a1301354fad44c745b25cfc80ba0763a88
daejo <daejo@icloud.com>
2020-09-29 00:56:11 -0700

    adds post request

--- server.js
+++ server.js
@@ -74,6 +74,12 @@ app.get('/api/animals/:id', (req, res) => {
     }
 });
 
+app.post('/api/animals', (req, res) => {
+    // req.body is where our incoming content will be
+    console.log(req.body);
+    res.json(req.body);
+});
+
 //SHOULD BE AT THE END OF CODE, 3001 is localport//
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
