442fefcb3e91d584783b2fb369debd35ce62cc25
daejo <daejo@icloud.com>
2020-09-29 15:25:05 -0700

    fixes linking issues

--- server.js
+++ server.js
@@ -12,13 +12,12 @@ const htmlRoutes = require('./routes/htmlRoutes');
 const { animals } = require('./data/animals.json');
 const express = require("express");
 const PORT = process.env.PORT || 3001;
-const fs = require('fs');
-const path = require('path');
 const app = express();
 
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true })); //takes incoming POST data and converts it to key/value pairings that can be accessed in the req.body object
 // parse incoming JSON data
+app.use(express.static("public"));
 app.use(express.json());
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
