8af1430d71050d737b5208a7bf7ccaf3d9769955
daejo <daejo@icloud.com>
2020-09-28 22:58:30 -0700

    links animals api to script

--- server.js
+++ server.js
@@ -2,6 +2,17 @@
 // npm init -y
 // # You can use `npm i` as a shortcut for `npm install`
 // npm i express
-const express = require("express")
+const { animals } = require('./data/animals.json');
+const express = require("express");
 
 const app = express();
+
+
+app.get('/api/animals', (req, res) => {
+    res.json(animals);
+})
+
+//SHOULD BE AT THE END OF CODE, 3001 is localport//
+app.listen(3001, () => {
+    console.log(`API server now on port 3001!`);
+});
